57acb58396092b44cec3aff67c2439ac3d70616a
Sang jin Lee <w1004123@gmail.com>
2023-04-13 20:59:45 +0900

    any는 쓰면 안돼

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 var 이름 = 'kim';
 var 나이 = 50;
 var marry = undefined;
-var 회원 = ['kim', 'park'];
+var 회원들 = { member1: 'kim', member2: 'park' };
+var namee = "이상진";
+var agee = 18;
+var jumin = 20060202;
+var singer = { singer: "빅뱅", song: "거짓말" };
+var project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true,
+};
+var 회원 = 123;
--- index.ts
+++ index.ts
@@ -2,4 +2,28 @@ let 이름 :string = 'kim';
 let 나이 = 50;
 let marry :undefined = undefined;
 
-let 회원 : string[] = ['kim', 'park']
+let 회원들 : {member1 : string, member2 : string} = { member1: 'kim', member2: 'park'};
+
+const namee :string ="이상진";
+const agee :number = 18;
+const jumin :number = 20060202;
+
+let singer : {singer : string, song: string} = {singer : "빅뱅", song : "거짓말"};
+
+let project : {
+  member: string[],
+  days: number,
+  started: boolean,
+} = {
+  member : ['kim','park'],
+  days : 30,
+  started : true,
+}
+
+let 회원 : (number | string) = 123;
+
+let obj : {a : string | number} = {a : '123'}
+
+let 이르음 : any;
+이르음 = 123;
+이르음 = [];
