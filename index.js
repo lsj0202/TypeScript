95799e5bf31a8e1d356c56c6789805e7e7aab314
Sang jin Lee <w1004123@gmail.com>
2023-04-13 20:34:42 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+var 이름 = 'kim';
+var 나이 = 50;
+var marry = undefined;
+var 회원 = ['kim', 'park'];
---
+++ index.ts
@@ -0,0 +1,5 @@
+let 이름 :string = 'kim';
+let 나이 = 50;
+let marry :undefined = undefined;
+
+let 회원 : string[] = ['kim', 'park']
